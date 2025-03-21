786dd2c797915888edd181b0b8ca5a9533fcfc9b
Nikhil Ponduri <nikhil@surveysparrow.com>
2025-03-21 15:52:45 +0530

    added API token

--- src/axios.ts
+++ src/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: "http://api.thrivesparrow.test/api/developer",
   headers: {
-    Authorization: `Bearer pv_aKxWN9Sc6c7k2zgQotoo1UwoxeEN4b78R7NwMkqzL1SDFx9pK38LXo11nmWmR0cO`,
+    Authorization: `Bearer ${process.env.API_TOKEN}`,
   },
 });
