9b48ba7035e41ec3c3645b64261cb876c7c7ca26
Nikhil Ponduri <nikhil@surveysparrow.com>
2025-03-21 16:04:04 +0530

    Cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,11 @@ import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
