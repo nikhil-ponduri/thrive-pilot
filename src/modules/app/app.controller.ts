df35a33475cb30d86f1a6b9011ac3be23ca94978
Nikhil Ponduri <nikhil@surveysparrow.com>
2025-03-21 18:02:31 +0530

    Changes

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -2,15 +2,14 @@ import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import BaseAgent from '../agents';
 import ResponseInterceptor from '../agents/interceptors/response.interceptor';
+import { AIMessage, BaseMessage } from '@langchain/core/messages';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Post("/chat")
-  async getHello(@Body() body: { message: string }): Promise<{
-    response: string;
-  }> {
+  async getHello(@Body() body: { message: string }): Promise<any> {
     try {
       if (!body?.message) {
         return { response: 'No message provided' };
@@ -26,9 +25,13 @@ export class AppController {
           content: body.message
         }
       ]);
-      // return result.messages;
-      const response = await ResponseInterceptor.intercept(result.messages);
-      return { response };
+      const filteredMessages = result.messages.filter((message: any) => {
+        // make sure content is not null and tool_calls is empty and tool_call_id is null
+        return !!message.content && !message.tool_calls?.length && !message.tool_call_id && message.type === 'ai'
+      });
+     
+      const contents = filteredMessages.map((message: any) => message.content);
+      return { contents };
     } catch (error) {
       console.error(error);
       throw new Error('Error');
